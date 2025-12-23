7855a7b7e2678bd2da7cb434d4f87cbe64590246
Rasif Al Saif <rasifalsaif@gmail.com>
2025-12-23 16:35:43 +0600

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from "../assets/logo.png"
+const Header = () => {
+    return (
+        <div className="flex justify-center flex-col items-center gap-3">
+            <img className="w-87.5" src={logo} alt="" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+        </div>
+    );
+};
+
+export default Header;
